7cfc3654fa9192032a98f7aa8014e0e27377ff71
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-11-05 11:01:13 +0100

    Agrego los metodos en los modelos para crear las relaciones entre las tablas.
    En los modelos: Cocktail.php Ingredient.php y User.php le agrego los metodos para crear las relaciones entre las tablas en la base de datos

--- Sprint4/app/Models/Cocktail.php
+++ Sprint4/app/Models/Cocktail.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Cocktail extends Model
-{
+class Cocktail extends Model {
+    
     public $timestamps = false;
 
-    //
+    public function usuario() {
+      
+        return $this->belongsTo(User::class, 'usuario_id'); //belongTo significa que cada Cocktail pertenece a un usuario
+    }
+
+    public function ingredients() {
+     
+        return $this->belongsToMany(Ingredient::class, 'cocktail_ingredient') //belongsToMany es la relacion muchos a muchos
+                    ->withPivot('cantidad', 'unidad'); // aqui pongo que ademas esta tabla tiene columnas extras ademas de las relaciones
+    }
+
+
 }
--- Sprint4/app/Models/Ingredient.php
+++ Sprint4/app/Models/Ingredient.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Ingredient extends Model
-{
+class Ingredient extends Model {
+    
     public $timestamps = false;
-    //
+    
+
+    public function cocktails() {
+      
+        return $this->belongsToMany(Cocktail::class, 'cocktail_ingredient')
+                  ->withPivot('cantidad', 'unidad');
+    }
+
 }
