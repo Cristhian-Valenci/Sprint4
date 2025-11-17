0d864ea04cd2d423179914601271c448d5715bcb
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-11-17 14:02:12 +0100

    Agrego la vista de ingredientes terminada
    Agrego la vista de ingredientes terminada,
    tambien agrego un archivo de authservidesProvider para la autorizacion en la policy que me faltaba,
    y la tabla migracion para crear una columna de user_id a ingredientes, para poder organizar la lista de ingredientes.

---
+++ Sprint4/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\Ingredient;
+use App\Policies\IngredientPolicy;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     */
+    protected $policies = [
+        Ingredient::class => IngredientPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+    }
+}
