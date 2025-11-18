8611b79acb86be89020cc112c06004eaf0f7ecd8
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-11-18 12:30:01 +0100

    Agrego cambios en la vista de crear coctel, un archivo nuevo de js, y actualizo el controlador

---
+++ Sprint4/resources/js/cocktail-crate.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let ingredienteIndex = 1;
+
+    document.getElementById('add-ingrediente').addEventListener('click', function() {
+        const container = document.getElementById('ingredientes-container');
+        const newRow = document.querySelector('.ingrediente-row').cloneNode(true);
+        // actualizar los nombres de los inputs
+        newRow.querySelectorAll('select, input').forEach(input => {
+            const name = input.getAttribute('name');
+            const newName = name.replace(/\d+/, ingredienteIndex);
+            input.setAttribute('name', newName);
+            if(input.tagName === 'SELECT') input.selectedIndex = 0;
+            else input.value = '';
+        });
+
+        container.appendChild(newRow);
+        ingredienteIndex++;
+    });
+      // eliminar fila
+    document.getElementById('ingredientes-container').addEventListener('click', function(e) {
+        const btn = e.target.closest('.remove-ingrediente'); // Buscar el botón más cercano al elemento clickeado
+        if (!btn) return;
+
+        const row = btn.closest('.ingrediente-row');
+        const rows = document.querySelectorAll('.ingrediente-row');
+        if (rows.length > 1 && row) row.remove();
+    });
+});
--- Sprint4/resources/views/cocktails/create.blade.php
+++ Sprint4/resources/views/cocktails/create.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2>Crear Cóctel</h2>
-    </x-slot>
+    <x-subheader title="Crear cóctel">
+    </x-subheader>
 
     @if ($errors->any())
     <div class="mb-4 p-2 bg-red-200 text-red-800 rounded">
@@ -20,31 +19,34 @@
 
 
 
-    <div class="max-w-4xl mx-auto bg-white p-6 rounded shadow">
+    <div class="max-w-4xl mx-auto bg-white p-6 rounded shadow mt-8">
         <form action="{{ route('cocktails.store') }}" method="POST">
             @csrf
 
             <!-- Nombre del cóctel -->
             <div class="mb-4">
-                <label for="nombre">Nombre:</label>
-                <input type="text" id="nombre" name="nombre" class="border rounded w-full p-2" required>
+                <label for="nombre" class="font-bold">Nombre:</label>
+                <input type="text" id="nombre" name="nombre" class="border rounded w-full p-2"
+                 placeholder = "Escribe el nombre del cóctel" required>
             </div>
 
             <!-- Descripción -->
             <div class="mb-4">
-                <label for="descripcion">Descripción:</label>
-                <textarea id="descripcion" name="descripcion" class="border rounded w-full p-2" ></textarea>
+                <label for="descripcion" class="font-bold">Descripción:</label>
+                <textarea id="descripcion" name="descripcion" class="border rounded w-full p-2" 
+                placeholder = "Has una breve descripcion del cóctel (donde se creó, quien lo creó, por qué se creó, etc.)"></textarea>
             </div>
 
             <!-- Método de elaboración -->
             <div class="mb-4">
-                <label for="metodo_elaboracion">Método de elaboración:</label>
-                <textarea id="metodo_elaboracion" name="metodo_elaboracion" class="border rounded w-full p-2" ></textarea>
+                <label for="metodo_elaboracion" class="font-bold">Método de elaboración:</label>
+                <textarea id="metodo_elaboracion" name="metodo_elaboracion" class="border rounded w-full p-2" 
+                placeholder = "Aquí puedes poner con que método se elabora, en que vaso se sirve, que decoración lleva, etc."></textarea>
             </div>
 
             <!-- Ingredientes -->
             <div class="mb-4">
-                <label>Ingredientes:</label>
+                <label class="font-bold">Ingredientes:</label>
                 <div id="ingredientes-container">
 
                     <div class="ingrediente-row mb-2 flex gap-2">
@@ -67,7 +69,13 @@
                             <option value="cucharadas">cucharadas</option>
                         </select>
 
-                        <button type="button" class="remove-ingrediente bg-red-500 text-white px-2 rounded">Eliminar</button>
+                        <button type="button" 
+                                class="remove-ingrediente text-white px-3 py-1 rounded hover:bg-red-700 flex items-center justify-center">
+                                <img src="{{ asset('images/delete-buttom.png') }}" 
+                                alt="Eliminar"
+                                class="w-5 h-5">    
+                            </button>
+
                     </div>
 
                 </div>
@@ -77,39 +85,15 @@
                 </button>
             </div>
 
-            <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded">
-                Guardar
-            </button>
+            <div class="mt-4 text-right">
+               <button type="submit" class="bg-green-700 text-white px-4 py-2 rounded hover:bg-green-800">
+                   Guardar
+                </button>
+            </div>
         </form>
     </div>
 
-    <!-- JS para agregar/eliminar ingredientes -->
-    <script>
-        let ingredienteIndex = 1;
+    @vite('resources/js/app.js')
 
-        document.getElementById('add-ingrediente').addEventListener('click', function() {
-            const container = document.getElementById('ingredientes-container');
-            const newRow = document.querySelector('.ingrediente-row').cloneNode(true);
 
-            // actualizar los nombres de los inputs
-            newRow.querySelectorAll('select, input').forEach(input => {
-                const name = input.getAttribute('name');
-                const newName = name.replace(/\d+/, ingredienteIndex);
-                input.setAttribute('name', newName);
-                if(input.tagName === 'SELECT') input.selectedIndex = 0;
-                else input.value = '';
-            });
-
-            container.appendChild(newRow);
-            ingredienteIndex++;
-        });
-
-        // eliminar fila
-        document.getElementById('ingredientes-container').addEventListener('click', function(e) {
-            if(e.target.classList.contains('remove-ingrediente')) {
-                const rows = document.querySelectorAll('.ingrediente-row');
-                if(rows.length > 1) e.target.closest('.ingrediente-row').remove();
-            }
-        });
-    </script>
 </x-app-layout>
